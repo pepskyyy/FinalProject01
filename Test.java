94574e2e4ae83c02cf4e99124b6553546e33565f
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2025-10-05 20:32:20 +0700

    Aku akmal

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         // Your code here
         System.out.println("Halo Dunia, ini ahmad rifqi");
+        System.out.println("Halo world, this is aku me akmal fitrahari");
     }
 }
