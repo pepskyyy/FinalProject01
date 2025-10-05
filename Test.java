fb272944366014d94b67dd86e853cff587606519
pepskyyy <peduutrifqi@gmail.com>
2025-10-05 20:35:07 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         // Your code here
-        System.out.println("Halo Dunia, ini ahmad rifqi");
+        System.out.println("Halo Dunia, ini this is saya me Ahmad Rifqi");
         System.out.println("Halo world, this is aku me akmal fitrahari");
     }
 }
